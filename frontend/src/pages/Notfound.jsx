063bdb653ba7faae70790c69b3e5325b5dcbb966
ifeco445 <ifeanyidavid445@gmail.com>
2024-06-25 12:44:52 +0100

    first commit

---
+++ frontend/src/pages/Notfound.jsx
@@ -0,0 +1,11 @@
+function NotFound() {
+    return(
+        <div>
+            <h1>404 Not Found</h1>
+            <p>The page your Looking for does not exist</p>
+            </div>
+    )
+   
+}
+
+export default NotFound
